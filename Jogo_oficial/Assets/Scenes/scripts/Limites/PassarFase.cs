adf27c80829df125640aba118e40d9cee07b415f
livegpc <gugupcoutinho@hotmail.com>
2019-05-25 22:53:06 -0300

    - Passador de fases (para o menu); - Passagem progressiva de lixo; - Ajustes gerais;

--- Jogo_oficial/Assets/Scenes/scripts/Limites/PassarFase.cs
+++ Jogo_oficial/Assets/Scenes/scripts/Limites/PassarFase.cs
@@ -1,25 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
 
-public class Score : MonoBehaviour
+public class PassarFase : MonoBehaviour
 {
-    public static int pontos;
-    Text text;
-
+    float limite = 60;
     // Start is called before the first frame update
     void Start()
     {
-        text = GetComponent<Text>();
-        pontos = 0;
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = "Pontos:" + pontos;
-        
+        if (Time.time >= limite)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
